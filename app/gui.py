d457b964278b4b177b3d2b0c4f689bed9b84feb8
Davi Lucas <davibezerra123457@gmail.com>
2023-11-28 18:19:32 -0300

    testando temas

--- app/gui.py
+++ app/gui.py
@@ -1,7 +1,11 @@
 import customtkinter as Ct
 
+Ct.set_default_color_theme("dark-blue")
+Ct.set_appearance_mode("dark")
+
 window = Ct.CTk()
 window.geometry("500x300")
+window.title("calculator")
 
 text = Ct.CTkLabel(window, 
                    text="fazer login")
